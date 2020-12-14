4b5f22a0b156b197eb7e352bace0d2a5f95e1ab7
Darren Soothill <darren@soothill.com>
2020-12-14 04:56:31 +0000

    CreateRBD

---
+++ CreateRBD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create RBD Devices
+
+Poolname=$1
+NumberOfDevices=$2
+SizeOfDevice=$3
+
+if [ "$#" -eq "0" ];
+then
+  echo
+  echo
+  echo Parameters need :-
+  echo
+  echo PoolName :- Name of pool to be deleted
+  echo NumberOfDevices :- Number of RBD devices to create
+  echo SizeOfDevice :- Size in Gbytes
+  echo
+fi
+
+rbd pool init $Poolname
+
+rbd create "$Poolname"1 --size $SizeOfDevice -p $Poolname
+
+
