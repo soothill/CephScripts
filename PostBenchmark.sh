73b67cc5b546ba323ac227f4720354086a6d74e6
Darren Soothill <darren@soothill.com>
2020-12-14 05:44:46 +0000

    Pre and post scripts

---
+++ PostBenchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Commands to run after a benchmark
+
+ceph osd set scrub
+ceph osd set deep-scrub
---
+++ PreBenchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Commands to run prior to a benchmark
+
+ceph osd set noscrub
+ceph osd set nodeep-scrub
