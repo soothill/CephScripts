e383d714d11ff3d1417181d6240c715f629d8cce
Darren Soothill <darren@soothill.com>
2020-12-14 05:05:29 +0000

    Update

--- CreateRBD.sh
+++ CreateRBD.sh
@@ -22,7 +22,7 @@ fi
 rbd pool init $Poolname
 
 counter=1
-while [ $counter -le $NumberOfDevices]
+while [ $counter -le $NumberOfDevices ]
 do
     rbd create $Poolname+$Counter --size $SizeOfDevice -p $Poolname
     ((counter++))
